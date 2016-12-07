2e923c4776e05f23f393d77ffa21e354c3b0d98d
giercko <giercko.contreras.12@sansano.usm.cl>
2016-12-07 11:39:19 -0300

    agregado enviar notificacion de si se ocupa, probando formato de fecha

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 var handler = {}
-handler["/"] = requestHandlers.formulario;
-handler["/formulario"] = requestHandlers.formulario;
+handler["/"] = requestHandlers.ocupado;
+handler["/formulario"] = requestHandlers.notificacion_personalizada;
 handler["/enviar"] = requestHandlers.enviar;
+handler["/enviar_personalizado"] = requestHandlers.enviar_personalizado;
 
 server.start(router.route, handler);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,14 +1,14 @@
 var querystring = require("querystring");
 var notify = require("./notify");
 
-function formulario(response) {
-    console.log("Handler for request 'a_donde_vas' dispatched.");
+function notificacion_personalizada(response) {
+    console.log("Handler for request 'notificacion_personalizada' dispatched.");
     var html = '<html>'+
         '    <head>'+
         '        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'+
         '    </head>'+
         '    <body>'+
-        '        <form action="/enviar" method="post">'+
+        '        <form action="/enviar_personalizado" method="post">'+
         '            <p><label for="regID">Escribe el id del dispositivo: </label></p>'+
         '            <input type="text" name="regID" id="regID" />'+
         '            <p><label for="title">Escribe el titulo de la notificacion: </label></p>'+
@@ -24,9 +24,26 @@ function formulario(response) {
     response.end();
 }
 
+function ocupado(response){
+	console.log("Handler for request 'notificacion_ocupado' dispatched.");
+    var html = '<html>'+
+        '    <head>'+
+        '        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'+
+        '    </head>'+
+        '    <body>'+
+        '        <form action="/enviar" method="post">'+
+        '            <p><label for="regID">Escribe el id del dispositivo: </label></p>'+
+        '            <input type="text" name="regID" id="regID" ></textarea>'+
+        '            <p><input type="submit" value="Enviar" /></p>'+
+        '        </form>'+
+        '    </body>'+
+        '</html>';
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(html);
+    response.end();
+}
 
-
-function enviar(response, postData) {
+function enviar_personalizado(response, postData) {
     postData = querystring.parse(postData);
     console.log(postData);
 	notify.send_push(postData.nullregID, postData.msg, postData.title);
@@ -36,6 +53,19 @@ function enviar(response, postData) {
     response.write("<p>Notificacion enviada correctamente</p>");
     response.end();
 }
+function enviar(response, postData) {
+    postData = querystring.parse(postData);
+    console.log(postData);
+	a = new Date();
+	notify.send_push(postData.nullregID, a, "Usaste un almuerzo");
+	
+    console.log("Handler for request 'enviar' dispatched.");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<p>Notificacion enviada correctamente</p>");
+    response.end();
+}
 
-exports.formulario = formulario;
+exports.ocupado = ocupado;
+exports.notificacion_personalizada = notificacion_personalizada;
 exports.enviar = enviar;
+exports.enviar_personalizado = enviar_personalizado;
